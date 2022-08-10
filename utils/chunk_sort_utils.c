4065be296e59c24a9e0c3b134d40c3aa9edfb4c2
Achraf Faris <afaris@student.1337.ma>
2022-08-10 10:57:21 +0000

    bug fixed while updating reference indexes

--- utils/chunk_sort_utils.c
+++ utils/chunk_sort_utils.c
@@ -9,7 +9,7 @@ void    update_reference_indexes(t_stack *s, int lt, int gt)
             s->reference->start = 0;
         s->reference->least_size = (s->reference->middle + 1) - s->reference->start;
     }
-    else if (gt == s->reference->greatest_size)
+    if (gt == s->reference->greatest_size)
     {
         s->reference->end += s->offset; 
         if (s->reference->end >= s->reference->size)
